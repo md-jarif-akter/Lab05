439cf752c721feea6514903548356f22ef6e0b40
md-jarif-akter <akter15-5156@diu.edu.bd>
2025-02-18 09:56:20 +0600

    C2

--- lib/main.dart
+++ lib/main.dart
@@ -71,23 +71,27 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Row(
+        children: [
+          Text(
+            "Hi, ",
+            style : TextStyle(
+              color: Colors.yellow, 
+              fontsize: 50,
+            ),
+            ),
+            Text(
+              "Flutter.",
+              style : TextStyle(
+                color: Colors.red, 
+              fontsize: 20,
+            ),
+            ),
+              )
+        ]
+      
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
